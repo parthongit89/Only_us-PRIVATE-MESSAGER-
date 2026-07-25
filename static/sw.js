03bf60fb8ff6c66caff5410f53b0baef4367687e
parthongit89 <sonavaneparthgit@gmail.com>
2026-07-25 18:06:51 +0530

    Upgrade application to Progressive Web App (PWA) supporting home screen installation with custom app logo

---
+++ static/sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'onlyus-pwa-v1';
+const ASSETS = [
+  '/',
+  '/static/manifest.json',
+  '/static/icons/icon-192.png',
+  '/static/icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
